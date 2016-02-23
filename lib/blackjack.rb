7ac6b3a92d90a8159094dd725a80422e5b191ab5
Erica <erica@Ericas-Mac-mini.local>
2016-02-23 17:42:54 -0500

    finally done

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,56 @@
 def welcome
-  # code #welcome here
+    # code #welcome here
+    puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+    # code #deal_card here
+    card = rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+    puts "Your cards add up to #{card_total}"
+    # code #display_card_total here
 end
 
 def prompt_user
-  # code #prompt_user here
+    puts "Type 'h' to hit or 's' to stay"
+    
+    # code #prompt_user here
 end
 
 def get_user_input
-  # code #get_user_input here
+    gets.chomp
+    # code #get_user_input here
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+    # code #end_game here
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+    sum = deal_card() + deal_card()
+    display_card_total(sum)
+    sum
 end
 
-def hit?
-  # code hit? here
+
+def hit?(card_total)
+    # code hit? here
+    prompt_user
+    choice = get_user_input
+    card = 0
+    if choice == 'h'
+        card = deal_card
+    end
+    card_total + card
+    
 end
+
 
 def invalid_command
-  # code invalid_command here
+    # code invalid_command here
 end
 
 #####################################################
@@ -39,6 +58,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+    welcome
+    sum = initial_round
+    until sum > 21
+        sum = hit?(sum)
+        display_card_total(sum)
+    end
+    end_game(sum)
 end
-    
